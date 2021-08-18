15d3121af472e6bb49b96afbda044675c7b500eb
mari-arantes <88901582+mari-arantes@users.noreply.github.com>
2021-08-18 19:30:17 -0300

    Primeiro commit
    Pasta do curso CTD - Programação imperativa

---
+++ Programação Imperativa/Aulas/Aula04/Cotacao/teste/script.js
@@ -0,0 +1,28 @@
+/*
+    Alerts
+    Documentação: https://developer.mozilla.org/pt-BR/docs/Web/API/Window/alert
+
+    alert(texto|valor);
+
+    Prompts
+    Documentação: https://developer.mozilla.org/pt-BR/docs/Web/API/Window/prompt
+
+    prompt(texto, valor);
+
+    Confirm
+    Documentação: https://developer.mozilla.org/pt-BR/docs/Web/API/Window/confirm
+
+    confirm(mensagem);
+*/
+
+// Comentário de única linha.
+
+/*
+Comentário de múltiplas linhas.
+Utilizada principalmente em documentações de código.
+*/
+
+var saudacao = 'Olá';
+var nome = prompt('Qual o seu nome?');
+
+alert(saudacao + " " + nome);
